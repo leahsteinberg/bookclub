99bfdeef3ae060c6d71dedb89b494c7b5ae64242
leahsteinberg <leahsteinberg1@gmail.com>
2015-09-02 15:48:54 -0400

    fixing gitignore

--- secrets.js
+++ secrets.js
@@ -1,3 +1,7 @@
-var awsId = "KIAIVNCRRDZVOVSAZWQ";
+var awsId = "AKIAIVNCRRDZVOVSAZWQ";
 
 var awsSecret = "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";
+var awsAssociateTag = "leahstei-20";
+
+
+module.exports= {awsId: awsId, awsSecret: awsSecret, awsAssociateTag: awsAssociateTag};
