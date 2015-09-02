e803627437932d338bf90c776e3bb882aef3dc45
Leah Steinberg <leah_steinberg@brown.edu>
2015-09-02 15:48:27 -0400

    Delete secrets.js

--- secrets.js
+++
@@ -1,7 +0,0 @@
-var awsId = "AKIAIVNCRRDZVOVSAZWQ";
-
-var awsSecret = "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";
-var awsAssociateTag = "leahstei-20";
-
-
-module.exports= {awsId: awsId, awsSecret: awsSecret, awsAssociateTag: awsAssociateTag};
